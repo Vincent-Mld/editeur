73435124e8d58b2c243210291e3eadcbb42003e2
Meliand Vincent <vincent.meliand@etu.umontpellier.fr>
2024-09-06 09:50:36 +0200

    ex 3 fini

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -11,14 +11,18 @@ public class CommandeRemplacer extends CommandeDocument {
 
     @Override
     public void executer() {
-        if(parameters.length < 4) {
+        if(parameters.length < 3) {
             System.err.println("Format attendu :  remplacer;debut;fin;chaine\n");
             return;
         }
         int debut = Integer.parseInt(parameters[1]);
         int fin = Integer.parseInt(parameters[2]);
-        String chaine = parameters[3];
-        this.document.remplacer(debut,fin,chaine);
+        if(parameters.length == 4) {
+            String chaine = parameters[3];
+            this.document.remplacer(debut,fin,chaine);
+        } else {
+            this.document.remplacer(debut, fin, "");
+        }
         super.executer();
     }
 
